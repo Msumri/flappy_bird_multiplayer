68cbc2f9806a4fdb00d95a2e6cde212b19868834
mohammed mac <malhart28@gmail.com>
2026-04-14 14:26:33 -0500

    almost ready

--- bird.gd
+++ bird.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -285.0
 @onready var sprite_2d: AnimatedSprite2D = %Sprite2D
 @export var playable:=false
 func _enter_tree() -> void:
